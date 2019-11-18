9668851dac02a8adb354cccaa8d42acd151155fd
blockhead4 <blockhead4@naver.com>
2019-11-18 17:04:29 +0900

    commit

--- memo_app/firenote/app/src/main/java/com/jwp/firenote/AuthActivity.java
+++ memo_app/firenote/app/src/main/java/com/jwp/firenote/AuthActivity.java
@@ -1,14 +1,107 @@
 package com.jwp.firenote;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.SigningInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class AuthActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.internal.ConnectionCallbacks;
+import com.google.android.gms.common.api.internal.OnConnectionFailedListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class AuthActivity extends AppCompatActivity
+        implements GoogleApiClient.OnConnectionFailedListener {
+
+    private SignInButton mSignInBtn;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        mSignInBtn = (SignInButton) findViewById(R.id.sign_in_btn);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build();
+
+
+
+        mSignInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(intent, 100);
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            GoogleSignInResult result
+                    = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            GoogleSignInAccount account = result.getSignInAccount();
+            if(result.isSuccess()) {
+                firebaseWidthGoogle(account);
+            } else {
+                Toast.makeText(this, "인증에 실패하였습니다", Toast.LENGTH_LONG).show();
+            }
+
+
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "인증에 실패하였습니다", Toast.LENGTH_LONG).show();
+    }
+
+    private void firebaseWidthGoogle(GoogleSignInAccount account) {
+        AuthCredential credential
+                = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+
+        Task<AuthResult> authResultTask
+                = mFirebaseAuth.signInWithCredential(credential);
+
+        authResultTask.addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                startActivity(new Intent(AuthActivity.this, MainActivity.class));
+                finish();
+            }
+        });
     }
 }
